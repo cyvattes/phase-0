ee2c2c84f3b40e8baa9016c84b7bd4f1f42c2c97
[Cyrus Vattes] <[cy.vattes@gmail.com]>
2016-01-24 21:36:36 +0000

    Add and update files for challenge 4.5

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,19 @@
+# Calculate a Grade
+
+# I worked on this challenge [by myself, with: John Maguire].
+
+
+# Your Solution Below
+
+def get_grade(grade)
+  if 90 <= grade
+    return "A"
+  elsif 80 <= grade
+    return "B"
+  elsif 70 <= grade
+    return "C"
+  elsif 60 <= grade
+    return "D"
+  else return "F"
+  end
+end
